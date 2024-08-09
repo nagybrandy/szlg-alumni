2de1659b09c1cfb279b67295a2003b5bb3dc6fbd
Pstas <ballabtnd@gmail.com>
2024-08-09 10:49:28 +0200

    chore: Update page layout to use full screen height

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
         <img src="/circles.svg" className="absolute  mix-blend-darken opacity-75 z-10 pointer-events-none" />
         <img src="/circles.svg" className="absolute  mix-blend-lighten opacity-100 z-10 pointer-events-none" />
       </div>
-      <div className="w-8/12 mx-auto bg-[#FCFFF8] min-h-full pb-[100px] p-5 rounded-[5em] flex flex-col text-center relative z-0">
+      <div className="w-8/12 mx-auto bg-[#FCFFF8] h-screen pb-[100px] p-5 rounded-[5em] flex flex-col text-center relative z-0">
         <div className="w-1/2 h-1/2 absolute top-0 translate-x-1/2 -translate-y-1/2 left-0 opacity-5 bg-[#28B61B] rounded-full mix-blend-darken z-10"></div>
         <img src="/logo-vertical.svg" alt="Logo" className="size-1/5 mx-auto" />
         <BlogContainer>
