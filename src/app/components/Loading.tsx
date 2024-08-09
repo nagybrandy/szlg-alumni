339a4a5beeddb3f68cdbb73c9ced733968d2e209
nagybrandy <57422699+nagybrandy@users.noreply.github.com>
2024-08-09 23:43:10 +0200

    index on landing-page,menu: 08c4237 Merge branch 'landing-page,menu' of https://github.com/nagybrandy/szlg-alumni into landing-page,menu

---
+++ src/app/components/Loading.tsx
@@ -0,0 +1,10 @@
+const Loading = () => {
+    return (
+      <div className="w-full h-full flex items-center justify-center ">
+        <img src="/logo.svg" alt="Loading..." className="w-32 h-32 animate-ping" />
+      </div>
+    );
+  }
+  
+  export default Loading;
+  
