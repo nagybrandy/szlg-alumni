acf5fb4e10de99f1f353eec99b325ff961950440
Tünde Lelkes <lelkes.tunde.21f@szlgbp.hu>
2024-08-09 10:55:55 +0200

    ..

---
+++ src/app/igazgato_post/components/forms.tsx
@@ -0,0 +1,16 @@
+import {
+    Card,
+    Input,
+    Checkbox,
+    Button,
+    Typography,
+  } from "@material-tailwind/react";
+   
+export function SimpleRegistrationForm() {
+return (
+    <div className="flex flex-col justify-center items-center w-full h-full gap-y-4">
+    
+        <Button color="green" variant="gradient" className="w-4/5">Bejelentkezés</Button>
+    </div>
+);
+}
