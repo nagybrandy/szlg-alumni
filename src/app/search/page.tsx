0c4d9c9c980c91fb4b4179ca321d5e21f7956afd
MatyasAkos <matyas.akos.23f@szlgbp.hu>
2024-08-09 10:56:00 +0200

    Did work on

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,3 +1,15 @@
+import { Avatar } from "../components/Avatar";
+import UsersContainer from "./components/UsersContainer";
+
 export default function SearchPage() {
-    
+    return (
+        <>
+            <div className="rounded bg-white">
+                <div className="flex items-center">
+                    <Avatar />
+                    <UsersContainer />
+                </div>
+            </div>
+        </>
+    )
 }
