0ceffca4226aab7ad7da0427c13a463debca9e86
Ogre05 <102312215+Ogre05@users.noreply.github.com>
2024-08-10 14:35:36 +0200

    landing patc

--- src/app/landing/page.tsx
+++ src/app/landing/page.tsx
@@ -1,7 +1,7 @@
 "use client"
 import Image from "next/image";
 import { SignUpButton } from "../components/SignUpButton";
-import { SignInButtonLanding } from "../components/SignInButtonLanding";
+import { SignInButtonLanding } from "./components/SignInButtonLanding";
 import Link from "next/link";
 
 export default function LandingPage() {
