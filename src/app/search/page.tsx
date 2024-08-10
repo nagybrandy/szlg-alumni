82657c253861d3553b15a3caaaf469b11cb19fec
Ogre05 <102312215+Ogre05@users.noreply.github.com>
2024-08-10 15:23:31 +0200

    search kinda working

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -5,6 +5,7 @@ import { PostIcon } from "../components/PostIcon";
 import { SearchIcon } from "../components/SearchIcon";
 import UsersContainer from "./components/UsersContainer";
 import React, { useState } from "react"
+import { SearchButtonSearch } from "./components/SearchButtonSearch";
 
 export default function SearchPage() {
     const [hideFilt, setHideFilt] = useState(true);
@@ -15,7 +16,7 @@ export default function SearchPage() {
             </div>
             <div className="w-8/12 mx-auto bg-[#FCFFF8] h-screen p-5 rounded-[5em] pb-[200px] mt-32 flex flex-col text-center relative z-0">
                 <div className="inline-flex inline-fill">
-                    <input type="text" className="block px-4 py-2 ml-12 border-2 text-sm text-gray-700 w-44 rounded-xl" role="menuitem" id="name-filt" placeholder="Név"></input>
+                    <input type="text" className="block px-4 py-2 ml-5 border-2 text-sm text-gray-700 w-44 rounded-xl" role="menuitem" id="name-filt" placeholder="Név"></input>
                     <input type="text" className="block px-4 py-2 ml-3 border-2 text-sm text-gray-700 w-44 rounded-xl" role="menuitem" id="year-filt" placeholder="Évfolyam"></input>
                     <input type="text" className="block px-4 py-2 ml-3 border-2 text-sm text-gray-700 w-44 rounded-xl" role="menuitem" id="class-filt" placeholder="Osztály"></input>
                     <input type="text" className="block px-4 py-2 ml-3 border-2 text-sm text-gray-700 w-44 rounded-xl" role="menuitem" id="job-filt" placeholder="Állás"></input>
@@ -42,12 +43,12 @@ export default function SearchPage() {
                             </div>
                         </div>
                     </div> */}
-                </div>
-                <div className="overflow-y-auto h-full rounded  px-5 py-2.5 mt-5">
-                    <UsersContainer />
-                </div>
+                        </div>
+                        <div className="overflow-y-auto h-full rounded  px-5 py-2.5 mt-5">
+                            <UsersContainer />
+                        </div>
 
-            </div>
-        </>
-    )
+                    </div>
+                </>
+                )
 }
