4a091f935010aabe92491c4f574f6e8a6d7e262c
nagybrandy <57422699+nagybrandy@users.noreply.github.com>
2024-08-10 16:19:56 +0200

    asd

--- src/app/components/SignInButton.tsx
+++ src/app/components/SignInButton.tsx
@@ -6,7 +6,7 @@ export const SignInButton = () => {
     const isLoggedIn = useSelector((state: RootState) => state.auth.isLoggedIn);
 
     return (
-        isLoggedIn ? (
+        !isLoggedIn ? (
         <button className="rounded-lg border-2 bg-green-900 border-green-800 w-[110%] h-8 text-[#D2F0CB] font-bold uppercase text-center text-sm">
             <span>Jelentkezz Be</span>
         </button>) : (
